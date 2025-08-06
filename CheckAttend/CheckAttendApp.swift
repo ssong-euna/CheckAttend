0406e3775fd2ece02b87700a57822e83c87ac3fa
euna.song <euna.song@homeplus.co.kr>
2025-08-06 10:22:42 +0900

    Initial Commit

---
+++ CheckAttend/CheckAttendApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckAttendApp.swift
+//  CheckAttend
+//
+//  Created by 송은아 on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CheckAttendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
